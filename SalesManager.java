fe7ed9405dfc151f9559894a4bc81ccf27be220a
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 16:05:09 +0800

    Create SalesManager.java

---
+++ SalesManager.java
@@ -0,0 +1,7 @@
+package managementsystem;
+
+public class SalesManager extends User {
+    public SalesManager(String username, String password) { //String userId, 
+        super(username, password, "Sales Manager"); //userId, 
+    }
+}
