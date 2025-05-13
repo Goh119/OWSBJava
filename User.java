f6ef211e4e50df0f0f33251ec18703614bb8447e
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 16:07:18 +0800

    User.java

---
+++ User.java
@@ -0,0 +1,44 @@
+package managementsystem;
+
+import java.io.Serializable;
+// abstract class User
+public class User implements Serializable {
+    
+//    private String userId;
+    private String username;
+    private String password;
+    private String role;
+    
+    public User(String username, String password, String role) { //String userId
+//        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+    
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+//    public String getUserId() {
+//        return userId;
+//    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+}
