0a0dfd39c3974e3af4b34bb69e1bbf9d09b12a1b
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 16:00:02 +0800

    InventoryManager.java

---
+++ InventoryManager.java
@@ -0,0 +1,7 @@
+package managementsystem;
+
+public class InventoryManager extends User {
+    public InventoryManager(String username, String password) { //String userId, 
+        super(username, password, "Inventory Manager"); //userId, 
+    }
+}
