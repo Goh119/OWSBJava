0be3a5f9f2ac1aff2707806febbfc19a4ea209a7
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 15:58:42 +0800

    FinanceManager.java

---
+++ FinanceManager.java
@@ -0,0 +1,7 @@
+package managementsystem;
+
+public class FinanceManager extends User {
+    public FinanceManager(String username, String password) { //String userId, 
+        super(username, password, "Finance Manager"); //userId, 
+    }
+}
