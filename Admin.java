84b642172defffea89b2121aab8686dc60f2710c
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 15:55:57 +0800

    Admin.java

---
+++ Admin.java
@@ -0,0 +1,7 @@
+package managementsystem;
+
+public class Admin extends User {
+    public Admin(String username, String password) {//String userId, 
+        super(username, password, "Admin"); //userId, 
+    }
+}
