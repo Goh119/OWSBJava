43edad6bb724acbc96bea2b13c739a51117b500e
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 16:02:46 +0800

    PurchaseManager.java

---
+++ PurchaseManager.java
@@ -0,0 +1,7 @@
+package managementsystem;
+
+public class PurchaseManager extends User {
+    public PurchaseManager(String username, String password) { //String userId, 
+        super(username, password, "Purchase Manager"); //userId, 
+    }
+}
