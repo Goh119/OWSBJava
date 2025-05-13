87c88d2b3292985545ef3f0ba3e2b9e52f72fabd
Goh119 <118416069+Goh119@users.noreply.github.com>
2025-05-13 16:02:18 +0800

    ManagementSystem.java

---
+++ ManagementSystem.java
@@ -0,0 +1,10 @@
+package managementsystem;
+
+public class ManagementSystem {
+
+    public static void main(String[] args) {
+        LoginPage login = new LoginPage();
+        login.setVisible(true);
+    }
+    
+}
